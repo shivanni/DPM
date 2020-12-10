4e335b24305efae03dac92acd97917c7d6d925fd
shivanni <nalawadeshivani98@gmail.com>
2020-12-10 13:27:24 +0530

    10decbakup

---
+++ DPM_Testing/DAL/DPMDal.cs
@@ -0,0 +1,35 @@
+﻿using DPM_Testing.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DPM_Testing.DAL
+{
+    public class DPMDal:DbContext
+    {
+        public DPMDal(DbContextOptions<DPMDal> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AddRuleModel> AddRuleModels { get; set; }
+
+        public DbSet<compressuredetails> compressuredetails { get; set; }
+
+        public DbSet<compressurewithclassification> compressureWithClassifications { get; set; }
+
+        public DbSet<ClassificationMaster> classiMaster { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AddRuleModel>().ToTable("addrulemodel");
+            modelBuilder.Entity<compressuredetails>().ToTable("compressuredetails");
+            modelBuilder.Entity<compressurewithclassification>().ToTable("compressurewithclassification");
+
+            modelBuilder.Entity<ClassificationMaster>().ToTable("classificationMaster");
+        }
+
+    }
+}
